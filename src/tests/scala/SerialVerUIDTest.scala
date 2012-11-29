944694c7f0968d0cf4d614494da36bae6831d970
David Skoog <dwskoog@gmail.com>
2012-11-29 13:18:40 -0500

    Scala compiler bug example
    javap -verbose on these shows the double loads.

---
+++ src/tests/scala/SerialVerUIDTest.scala
@@ -0,0 +1,16 @@
+package tests.scala
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: davidskoog
+ * Date: 11/29/12
+ * Time: 12:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@SerialVersionUID(0) class SerialVerUIDTest {
+
+}
+
+@SerialVersionUID(5599587822477417669L) class LongSerialVerUIDTest {
+
+}
