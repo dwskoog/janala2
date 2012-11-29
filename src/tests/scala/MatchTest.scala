0bf0a8b9f7d248c8d61b6d6ec85dc04542c155f7
David Skoog <dwskoog@gmail.com>
2012-11-28 23:17:38 -0500

    Pick up the branching inside a match case

--- src/tests/scala/MatchTest.scala
+++ src/tests/scala/MatchTest.scala
@@ -1,19 +1,23 @@
 package tests.scala
 
 import janala.Main
+import catg.CATG
 
 object MatchTest {
 
   def main(args: Array[String]) {
     val n = Main.readInt(0)
-    Main.MakeSymbolic(n)
-    val o = Main.readInt(0)
-    Main.MakeSymbolic(o)
-    val x:Any = (n,o)
-
+    println(n)
+    val o = Main.readInt(1)
+    println(o)
+    val x = (n,o)
     x match {
-      case (x,0) => println("Found a 0")
-      case (x:Int,y:Int) => if(y>10) println("Y is greater than 10") else println("Y is less than 10")
+      case (x,0) =>
+        println("Found a 0")
+      case p @ (x:Int,y:Int) =>
+        Main.MakeSymbolic(p._2)
+        Main.MakeSymbolic(y)
+        if(y>10) println("Y is greater than 10") else println("Y is less than 10")
       case _ => println("Fell out the bottom")
     }
   }
